4770790621973e93ddf2e94cb9d35808ecd69c55
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-09-28 12:19:01 -0400

    update progress

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,15 @@
-   
 const { User } = require('../models')
-const userController
+
+const userController = {  
+    getAllUsers(req, res) {
+    User.find({})
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+}
+
+
 module.exports = userController
